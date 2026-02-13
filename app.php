943d2ed532bec4b4f206cf7cb1d67aca8cd98409
Sadique Kokni <109825252+KokniDevloper@users.noreply.github.com>
2026-02-13 21:07:50 +0530

    Update file paths after directory restructure

--- app.php
+++ app.php
@@ -58,7 +58,7 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Newsletter Pro</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Frontend/styles/style.css">
 </head>
 
 <body>
@@ -68,7 +68,7 @@ mysqli_close($conn);
                 <h3>
                     <a class="primary-text" href="index.html">Newsletter Pro</a>
                 </h3>
-                <a href="logout.php">logout</a>
+                <a href="Backend/auth/logout.php">logout</a>
             </nav>
         </header>
         <div class="hero">
