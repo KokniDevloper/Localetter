388172339a52ad7b29201157e5f02ab4cd46c496
Sadique Kokni <109825252+KokniDevloper@users.noreply.github.com>
2026-04-16 15:47:02 +0530

    Add .gitignore and untrack secret.php

--- secret.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MAIL_FROM     = 'you@gmail.com'; // Your Gmail Address
-$MAIL_PASSWORD = 'xxxx xxxx xxxx xxxx'; // Gmail App Password
-$MAIL_NAME     = 'Your Name or Company Name';  // Display name in inbox
