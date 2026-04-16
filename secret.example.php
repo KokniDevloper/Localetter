38c73933497f00b369234e34ec12e725eb9c80c3
Sadique Kokni <109825252+KokniDevloper@users.noreply.github.com>
2026-04-16 22:33:49 +0530

    Add example configuration file for email credentials

---
+++ secret.example.php
@@ -0,0 +1,6 @@
+<?php
+$MAIL_FROM     = 'you@gmail.com'; // Your Gmail Address
+$MAIL_PASSWORD = 'xxxx xxxx xxxx xxxx'; // Gmail App Password
+$MAIL_NAME     = 'Your Name or Company Name';  // Display name in inbox
+
+//You will copy the contents of this file and create a new file named "secret.php" in root directory (i.e Localetter) and change the place holder values with actual credentials.
