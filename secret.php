ee0eb602901aa7c3bc4d29a02efe7d8f8d379383
Sadique Kokni <109825252+KokniDevloper@users.noreply.github.com>
2026-04-16 15:42:08 +0530

    Add secret.php for gmail credentials

---
+++ secret.php
@@ -0,0 +1,4 @@
+<?php
+$MAIL_FROM     = 'you@gmail.com'; // Your Gmail Address
+$MAIL_PASSWORD = 'xxxx xxxx xxxx xxxx'; // Gmail App Password
+$MAIL_NAME     = 'Your Name or Company Name';  // Display name in inbox
